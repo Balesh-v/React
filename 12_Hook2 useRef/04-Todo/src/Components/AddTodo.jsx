41e81dc1b28fcc7d8add73125462a11a0f9b3479
Balesh <baleshvarshney078@gmail.com>
2025-10-06 09:36:33 +0530

    modified useref file

--- 12_Hook2 useRef/04-Todo/src/Components/AddTodo.jsx
+++ 12_Hook2 useRef/04-Todo/src/Components/AddTodo.jsx
@@ -4,25 +4,30 @@ import './AddTodo.css';
 
 function AddTodo({onNewTodo}){
 
-    const [TodoName, setTodoName] = useState("");
-    const noofclick = useRef();
+    // const [TodoName, setTodoName] = useState("");
+    const noofupdate = useRef();
 
-    const handleChange = (event) => {
-        setTodoName(event.target.value);
-    }
+    // const handleChange = (event) => {
+    //     setTodoName(event.target.value);
+    //     noofupdate.current += 1;
+    //     console.log(noofupdate.current);
+    // }
 
     const handleAddTodo = (event) => {
       console.log(event)
       event.preventDefault();
+      const TodoName = noofupdate.current.value;
+      noofupdate.current.value = "";
          onNewTodo(TodoName);
-         setTodoName("");
+         console.log(TodoName);
+        //  setTodoName("");
     }
 
     return (
          <div className="container data ">
           <form className="call" onSubmit={handleAddTodo}>
             <div className="col-6 ">
-              <input value={TodoName} id='fed1' type="text" placeholder="Enter Todo Here" onChange={handleChange}/>
+              <input  id='fed1' type="text" ref={noofupdate} placeholder="Enter Todo Here" />
             </div>
             <div className="col-2">
               <button id='fed2' className="btn btn-success"><MdAdd /></button>
