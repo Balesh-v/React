83da6dc58f73be6d4ace85d9fdb59008d9b0d53f
Balesh <baleshvarshney078@gmail.com>
2026-01-13 11:50:41 +0530

    card-setion

--- practical/Practical-1/src/App.jsx
+++ practical/Practical-1/src/App.jsx
@@ -2,19 +2,32 @@ import Card from "./Components/card"
 import "./App.css"
 
 const App = () => {
-  
 
- 
-
-
+   const datas = [
+    {
+      Name : "GYANESH",
+      Role : "Officer",
+    } ,
+    {
+      Name : "Balesh",
+      Role : "Entrepinoures",
+    },
+    {
+      Name : "GYANESH",
+      Role : "Officer",
+    } ,
+    {
+      Name : "Balesh",
+      Role : "Entrepinoures",
+    }
+   ]
+      
   return (
     <>
-
     <div className="cotainer">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {datas.map((item)=>{
+       return   <Card datas={item.Name} Role={item.Role}/>
+      })}
     </div>
     </>
   )
--- practical/Practical-1/src/Components/Card.jsx
+++ practical/Practical-1/src/Components/Card.jsx
@@ -1,17 +1,50 @@
 
+import { GoArrowLeft } from "react-icons/go";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
-import MidLevel from "./MidLevel";
-import TopLevel from "./TopLevel";
+const Card = (props) => {
 
-const Card = () => {
   return (
     <>
-       <div className='Parent'>
-         <TopLevel/>
-         <MidLevel/>
-       </div>
-    </>
-  )
-}
+      <div className="Parent">
+         <div className="Top">
+                <div className="ar">
+                  <GoArrowLeft />
+                  <HiOutlineDotsVertical />
+                </div>
+                <div className="imgs">
+                  <div className="images">
+                    <img
+                      src="https://images.unsplash.com/photo-1693994866608-1159b9e28eb2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fHdoYXRzYXBwJTIwZHB8ZW58MHx8MHx8fDA%3D"
+                      alt="user"
+                    />
+                  </div>
+                  <div className="details">
+                    <h1>{props.datas}</h1>
+                    <p>{props.Role}</p>
+                  </div>
+                </div>
+              </div>
+        <div className="mid">
+        <div className="tag">
+          <FaTwitter />
+          <FaYoutube />
+          <FaFacebookF />
+          <FaInstagram />
+        </div>
 
-export default Card
+        <div className="btn">
+          <button>Subscribe</button>
+          <button>Message</button>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
