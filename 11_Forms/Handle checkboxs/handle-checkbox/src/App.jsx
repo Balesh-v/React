49a91e0a170e19df2df29c5024d808c6b7a0ed24
Balesh <baleshvarshney078@gmail.com>
2026-01-22 06:59:36 +0530

    create handleCheckbox file

---
+++ 11_Forms/Handle checkboxs/handle-checkbox/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>handle Checkbox</h1>
+
+      <label htmlFor="php">php</label>
+      <input type="checkbox" value="php" id="php" />
+      <br />
+      <br />
+      <label htmlFor="js">js</label>
+      <input type="checkbox" value="js" id="js" />
+      <br />
+      <br />
+      <label htmlFor="html">html</label>
+      <input type="checkbox" value="html" id="html" />
+      <br />
+      <br />
+      <label htmlFor="css">css</label>
+      <input type="checkbox" value="css" id="css" />
+      <br />
+      <br />
+    </div>
+  )
+}
+
+export default App
