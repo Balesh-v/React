3defc6579332bd3be9aad02409d3e21673ffae31
Balesh <baleshvarshney078@gmail.com>
2026-02-20 18:59:33 +0530

    todo-project

--- practical/prectical-4/Todo-project/src/components/Todo.jsx
+++ practical/prectical-4/Todo-project/src/components/Todo.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import '../Styles/Todo-style.css'
+import { useState } from 'react'
+import { FaCheck } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const Todo = () => {
+
+   const [input, setInput] = useState('')
+   const [task , setTask] = useState([])
+
+   const handleChange = (e) => {
+       setInput(e.target.value)
+   }
+
+   const handleSubmit = (e) => {
+       e.preventDefault()
+       if(task.includes(input)){
+           alert('Task already exists')
+           return
+       }
+       setTask([...task, input])
+       setInput('')
+   }
+
   return (
     <>
     <section className='todo-container'>
@@ -10,17 +32,30 @@ const Todo = () => {
             </div>
         </header>
         <section className='form'>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <input type="text" className='todo-input' autoComplete='off' />
+                    <input type="text" className='todo-input' value={input} autoComplete='off' onChange={handleChange} />
                 </div>
                 <div>
                     <button type='submit' className='todo-btn'>Add task</button>
                 </div>
             </form>
-          
+        <section className='todo-list'>
+            
+            <ul>
+                {task.map((item, index) => (
+                    <li className='todo-item' key={index}>
+                        <span>{item}</span>
+
+                        <div className='btn-container'>
+                           <button><FaCheck /></button>
+                           <button><MdDelete /></button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </section>
-      
+        </section>
     </section>
     </>
   )
